090f133a4c25328a4dc5b413e4c12d08372e5e25
Chris Hocking <latenitefilms@users.noreply.github.com>
2024-10-03 23:31:05 +1000

    Generated V6 Plugin
    - Using `dotnet LoupedeckPluginTool.dll generate -name=CommandPostPlugin`

---
+++ V6/src/CommandPostPlugin/CommandPostPlugin.cs
@@ -0,0 +1,35 @@
+namespace Loupedeck.CommandPostPlugin
+{
+    using System;
+
+    // This class contains the plugin-level logic of the Loupedeck plugin.
+
+    public class CommandPostPlugin : Plugin
+    {
+        // Gets a value indicating whether this is an API-only plugin.
+        public override Boolean UsesApplicationApiOnly => true;
+
+        // Gets a value indicating whether this is a Universal plugin or an Application plugin.
+        public override Boolean HasNoApplication => true;
+
+        // Initializes a new instance of the plugin class.
+        public CommandPostPlugin()
+        {
+            // Initialize the plugin log.
+            PluginLog.Init(this.Log);
+
+            // Initialize the plugin resources.
+            PluginResources.Init(this.Assembly);
+        }
+
+        // This method is called when the plugin is loaded.
+        public override void Load()
+        {
+        }
+
+        // This method is called when the plugin is unloaded.
+        public override void Unload()
+        {
+        }
+    }
+}
